6dc0a0adac0e58480ad4f9e1a2ad74d24d3044aa
Kalu Victor <victorbuikemdev@gmail.com>
2026-03-17 22:08:22 +0100

    fix: update database connection to use dynamic env import for DATABASE_URL

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,10 +1,10 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
-import { DATABASE_URL } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 
-// if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
-const client = postgres(DATABASE_URL);
+const client = postgres(env.DATABASE_URL);
 
 export const db = drizzle(client, { schema });
